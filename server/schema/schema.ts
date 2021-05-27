2831256dc843d5fb120f308cc18fdcb63cfeb61c
Erik <erikmat@berkeley.edu>
2021-05-27 14:35:18 -0700

    sample middleware routes

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -5,119 +5,128 @@ import {
   GraphQLList,
   GraphQLInt,
   GraphQLNonNull,
-  GraphQLInputObjectType
+  GraphQLInputObjectType,
 } from 'graphql';
 const db = require('../model');
 
-const UserType:any = new GraphQLObjectType({
-  name: "Users",
-  fields: ()=>({
-    user_id: {type: GraphQLInt},
-    name: {type: GraphQLString},
-    company_id: {type: GraphQLInt},
+const UserType: any = new GraphQLObjectType({
+  name: 'Users',
+  fields: () => ({
+    user_id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    company_id: { type: GraphQLInt },
     company: {
       type: CompanyType,
       resolve: async (user) => {
-      const result = await db.query(`SELECT * FROM company WHERE company_id = $1`, [user.company_id]);
-      return result.rows[0];
-      } 
-    }
-  })
-})
+        const result = await db.query(
+          `SELECT * FROM company WHERE company_id = $1`,
+          [user.company_id]
+        );
+        return result.rows[0];
+      },
+    },
+  }),
+});
 
-const CompanyType: any= new GraphQLObjectType({
-  name: "Companies",
-  fields: ()=>({
-    company_id: {type: GraphQLInt},
-    name: {type: GraphQLString},
-    description: {type: GraphQLString},
+const CompanyType: any = new GraphQLObjectType({
+  name: 'Companies',
+  fields: () => ({
+    company_id: { type: GraphQLInt },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
     employees: {
       type: GraphQLList(UserType),
-      resolve: async (company) =>{
-        const result = await db.query('SELECT * FROM public.user WHERE company_id = $1', [company.company_id]);
+      resolve: async (company) => {
+        const result = await db.query(
+          'SELECT * FROM public.user WHERE company_id = $1',
+          [company.company_id]
+        );
         return result.rows;
-      }
-    }
-  })
-})
+      },
+    },
+  }),
+});
 
 const ProjectType = new GraphQLObjectType({
-  name: "Projects",
-  fields: ()=>({
-    project_id: {type: GraphQLInt},
-    project_name: {type: GraphQLString},
-    company_id: {type: GraphQLInt},
-    project_description: {type: GraphQLString}
-  })
-})
+  name: 'Projects',
+  fields: () => ({
+    project_id: { type: GraphQLInt },
+    project_name: { type: GraphQLString },
+    company_id: { type: GraphQLInt },
+    project_description: { type: GraphQLString },
+  }),
+});
 
 const RootQueryType = new GraphQLObjectType({
   name: 'Query',
-  description : 'Root Query',
-  fields: ()=> ({
+  description: 'Root Query',
+  fields: () => ({
     company: {
       type: CompanyType,
-      description: "a single company",
+      description: 'a single company',
       args: {
-        company_id: { type: GraphQLInt}
+        company_id: { type: GraphQLInt },
       },
-      resolve: async(parent,args)=> {
-        const result =  await db.query(`SELECT * FROM company WHERE company_id = $1`, [args.company_id]);
+      resolve: async (parent, args) => {
+        const result = await db.query(
+          `SELECT * FROM company WHERE company_id = $1`,
+          [args.company_id]
+        );
         return result.rows[0];
-      }
+      },
     },
     //companies return call companies
     companies: {
       type: new GraphQLList(CompanyType),
-      description: "list of companies",
-      resolve: async()=> {
-        const result =  await db.query(`SELECT * FROM company`);
+      description: 'list of companies',
+      resolve: async () => {
+        const result = await db.query(`SELECT * FROM company`);
         return result.rows;
-      }
+      },
     },
     //users ruturn all usuers
     users: {
       type: new GraphQLList(UserType),
-      description: "list of users",
+      description: 'list of users',
       args: {
-        company_id: { type: GraphQLInt}
+        company_id: { type: GraphQLInt },
       },
-      resolve: async()=> {
-        const result =  await db.query("SELECT * FROM public.user");
+      resolve: async () => {
+        const result = await db.query('SELECT * FROM public.user');
         return result.rows;
-      }
-    },
-    project:{
-      type: ProjectType,
-      description: "project ",
-      args: {
-        project_id: {type: GraphQLInt}
       },
-      resolve: async (parent, args)=> {
-        const result = await db.query('SELECT * FROM project WHERE  project_id = $1', [args.project_id]);
+    },
+    project: {
+      type: ProjectType,
+      description: 'project ',
+      args: {
+        project_id: { type: GraphQLInt },
+      },
+      resolve: async (parent, args) => {
+        const result = await db.query(
+          'SELECT * FROM project WHERE  project_id = $1',
+          [args.project_id]
+        );
         return result.rows[0];
-      }
+      },
     },
     //projects return all projects
     projects: {
       type: new GraphQLList(ProjectType),
-      description: "list of projects",
+      description: 'list of projects',
       args: {
-        company_id: { type: GraphQLInt}
+        company_id: { type: GraphQLInt },
       },
-      resolve: async()=> {
-        const result =  await db.query("SELECT * FROM public.project");
+      resolve: async () => {
+        const result = await db.query('SELECT * FROM public.project');
         return result.rows;
-      }
-    }
-  })
-})
-
+      },
+    },
+  }),
+});
 
 const schema = new GraphQLSchema({
-  query: RootQueryType
-})
-
-
+  query: RootQueryType,
+});
 
 module.exports = schema;
--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 //the extra variables available from express define express types
+
 import path from 'path';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
@@ -9,8 +10,8 @@ import session from 'express-session';
 //if curious see package.json, had to install types for some of these dependencies to work with TS
 const db = require('./model');
 import { graphqlHTTP } from 'express-graphql';
-
-
+import { hasUncaughtExceptionCaptureCallback, nextTick } from 'process';
+import { graphql } from 'graphql';
 
 const schema = require('./schema/schema');
 
@@ -25,69 +26,90 @@ const PORT = process.env.PORT || 3000;
 //console.log(REDIS_PORT)
 //const client = redis.createClient(parseInt(REDIS_PORT));
 
-
-
 //we might need to configure this line somehow for users running behind a proxy
 // app.set('trust proxy', 1)
 
-const RedisStore = connectRedis(session)
+const RedisStore = connectRedis(session);
 //const redisClient = redis.createClient();
 const redisClient = redis.createClient({
   host: 'localhost',
-  port: 6379
-})
-
-app.use(session({
-  store: new RedisStore({
-    client: redisClient,
-    disableTouch: true}),
-  secret: 'foundAtlantis',
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 100 * 60 * 60 * 24 * 365 * 10,
-    httpOnly: true,
-    secure: false,
-    sameSite: 'lax' 
-},
-resave: false
-
-}))
-
-
-
-
-
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true,
-}));
-
-
-// app.use(express.static(path.join(__dirname, '../client')));
-// test route to check if our connection to the DB is working.
-app.get('/testing', (req, res) => {
-  const queryParams: string = 'Erik';
-  const queryString: string = `SELECT * FROM public.user
-                            WHERE name = $1`;
-
-  db.query(queryString, [queryParams], (err: object, result: any) => {
-    if (err) return res.send(err);
-    res.send(result.rows);
-  });
+  port: 6379,
 });
 
+app.use(
+  session({
+    store: new RedisStore({
+      client: redisClient,
+      disableTouch: true,
+    }),
+    secret: 'foundAtlantis',
+    saveUninitialized: false,
+    cookie: {
+      // 10 years
+      maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'lax',
+    },
+    resave: false,
+  })
+);
 
-interface Session {
-  username?: string
-}
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
-const initializeSession = (req: Request, res: Response , next: NextFunction) => {
-  (req.session as any ).username =  "username";
-  return next();
-}
- //next()
+const saveQuery = (req: Request, res: Response, next: NextFunction) => {
+  if (res.locals.query) next();
+  // save the response.data to res.locals.query
+  const query: string = '{companies{name}}';
+  graphql(schema, query).then((response) => {
+    console.log('saveQuery middleware responded with', response.data);
+    res.locals.query = response.data;
+    next();
+  });
+};
 
+app.get('/testing', saveQuery, (req, res, next) => {
+  // call saveQuery which will create an object and save it under
+  // res.locals.query
+  const pastQueries: any[] = [];
+  pastQueries.push(res.locals.query);
+  // push res.locals.query into the pastQueries array
+  // update the res.session.pastQueries to our new pastQueries
+  (req.session as any).pastQueries = pastQueries;
+  // tell client query has been saved
+  res.send('query should be in redis');
+});
 
+const checkRedis = (req: Request, res: Response, next: NextFunction) => {
+  // find session data pass to redis keys
+  console.log('session key is', req.session);
+  redisClient.keys('sess:*', (error, keys) => {
+    console.log('redis keys are', keys);
+    res.locals.query = keys;
+    next();
+  });
+  // find session key, and check for value of the key in redis
+  // if value contains query:
+  // save value to res.locals.query
+  // if not call next
+};
+
+app.get('/cachetest', checkRedis, saveQuery, (req, res, next) => {
+  // if res.locals exists don't make graphql query
+  res.send(res.locals.query);
+});
+
+const initializeSession = (req: Request, res: Response, next: NextFunction) => {
+  const pastQueries: any[] = [];
+  (req.session as any).pastQueries = pastQueries;
+  next();
+};
 
 app.get('/', initializeSession, (req: Request, res: Response) => {
   return res.status(200).sendFile(path.join(__dirname, './views/index.html'));
