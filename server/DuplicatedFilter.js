3f057d349a5443734cb24666df88f0c59aae3731
settnaing199 <settnaing199@gmail.com>
2021-06-11 13:15:08 -0400

    Duplicated FIlter

--- server/DuplicatedFilter.js
+++ server/DuplicatedFilter.js
@@ -2,13 +2,6 @@
 // Type JavaScript here and click "Run Code" or press Ctrl + s
 console.log('Hello, world!');
 
-// queryArr = [ "project_id", "project_name"]
-// jsonOjb= {
-//   "project_id": 1,
-//   "project_name": "iPhone",
-//   "company_id": 2,
-//   "project_description": null
-// }
 //you are inside an object
 const ObjectFilter = (jsonObj, queryArr)=>{
   let resultObj = {};
@@ -19,28 +12,18 @@ queryArr.forEach(field => {
   if(typeof field === "string"){
     resultObj[field] = jsonObj[field];
   }else{
-    const innerNested = {};
     const fieldName = Object.keys(field)[0];
-    innerNested[fieldName] = jsonObj[fieldName];
-    
     if(Array.isArray(jsonObj[fieldName])){
-      resultObj = {...resultObj, ...DuplicatedArr(jsonObj[fieldName], field[fieldName])};
-      // resultObj[fieldName] = DuplicatedArr(jsonObj[fieldName], field[fieldName]);
+    resultObj[fieldName] = jsonObj[fieldName].map(obj =>{
+      return ObjectFilter(obj, field[fieldName]);
+    });
+    }else{
+      resultObj[fieldName] = ObjectFilter(jsonObj[fieldName],field[fieldName] );
     }
   }
 })
 
  return resultObj;
-}
-
-//iterate through arr and push the new object  to result arr
-function DuplicatedArr(jsonArr,queryObj){
-  resultArr = [];
-  jsonArr.forEach(obj => {
-    resultArr.push(ObjectFilter(obj, queryObj));
-  })
-  return resultArr;
-  
 }
 
    //parent function that checks if nesting is array or object and triggers aplicable helper                        
@@ -57,7 +40,10 @@ function DuplicatedArr(jsonArr,queryObj){
       const valueOfQueryObj = queryObj[key];
       //check if value is a an array
       if(Array.isArray(valueJsonObj)){
-        result[key] = DuplicatedArr(valueJsonObj,valueOfQueryObj);
+        result[key] = valueJsonObj.map(obj =>{
+          return ObjectFilter(obj, valueOfQueryObj)
+        })
+        // result[key] = DuplicatedArr(valueJsonObj,valueOfQueryObj);
       }else{
         result[key] = ObjectFilter(valueJsonObj, valueOfQueryObj);
       }
@@ -128,13 +114,143 @@ companies: [
   },
 ],
 };
+
+const spaceX = {
+  launchesPast: [
+    {
+      mission_name: "Starlink-15 (v1.0)",
+      launch_date_local: "2020-10-24T11:31:00-04:00",
+      launch_site: {
+        site_name_long:
+          "Cape Canaveral Air Force Station Space Launch Complex 40",
+      },
+      links: {
+        article_link: null,
+        video_link: "https://youtu.be/J442-ti-Dhg",
+      },
+    },
+    {
+      mission_name: "Sentinel-6 Michael Freilich",
+      launch_date_local: "2020-11-21T09:17:00-08:00",
+      launch_site: {
+        site_name_long: "Vandenberg Air Force Base Space Launch Complex 4E",
+      },
+      links: {
+        article_link:
+          "https://spaceflightnow.com/2020/11/21/international-satellite-launches-to-extend-measurements-of-sea-level-rise/",
+        video_link: "https://youtu.be/aVFPzTDCihQ",
+      },
+    },
+    {
+      mission_name: "Crew-1",
+      launch_date_local: "2020-11-15T19:27:00-05:00",
+      launch_site: {
+        site_name_long: "Kennedy Space Center Historic Launch Complex 39A",
+      },
+      links: {
+        article_link:
+          "https://spaceflightnow.com/2020/11/16/astronauts-ride-spacex-crew-capsule-in-landmark-launch-for-commercial-spaceflight/",
+        video_link: "https://youtu.be/bnChQbxLkkI",
+      },
+    },
+    {
+      mission_name: "GPS III SV04 (Sacagawea)",
+      launch_date_local: "2020-11-05T18:24:00-05:00",
+      launch_site: {
+        site_name_long:
+          "Cape Canaveral Air Force Station Space Launch Complex 40",
+      },
+      links: {
+        article_link:
+          "https://spaceflightnow.com/2020/11/06/spacex-launches-gps-navigation-satellite-from-cape-canaveral/",
+        video_link: "https://youtu.be/wufXF5YKR1M",
+      },
+    },
+    {
+      mission_name: "Starlink-14 (v1.0)",
+      launch_date_local: "2020-10-24T11:31:00-04:00",
+      launch_site: {
+        site_name_long:
+          "Cape Canaveral Air Force Station Space Launch Complex 40",
+      },
+      links: {
+        article_link:
+          "https://spaceflightnow.com/2020/10/24/spacex-adds-another-60-satellites-to-starlink-network/",
+        video_link: "https://youtu.be/2gbVgTxLgN0",
+      },
+    },
+    {
+      mission_name: "Starlink-13 (v1.0)",
+      launch_date_local: "2020-10-18T08:25:00-04:00",
+      launch_site: {
+        site_name_long: "Kennedy Space Center Historic Launch Complex 39A",
+      },
+      links: {
+        article_link:
+          "https://spaceflightnow.com/2020/10/18/spacex-launches-another-batch-of-starlink-satellites/",
+        video_link: "https://youtu.be/UM8CDDAmp98",
+      },
+    },
+    {
+      mission_name: "Starlink-12 (v1.0)",
+      launch_date_local: "2020-10-06T07:29:00-04:00",
+      launch_site: {
+        site_name_long: "Kennedy Space Center Historic Launch Complex 39A",
+      },
+      links: {
+        article_link: null,
+        video_link: "https://youtu.be/8O8Z2yPyTnc",
+      },
+    },
+    {
+      mission_name: "Starlink-11 (v1.0)",
+      launch_date_local: "2020-09-03T08:46:00-04:00",
+      launch_site: {
+        site_name_long: "Kennedy Space Center Historic Launch Complex 39A",
+      },
+      links: {
+        article_link: null,
+        video_link: "https://youtu.be/_j4xR7LMCGY",
+      },
+    },
+    {
+      mission_name: "SAOCOM 1B, GNOMES-1, Tyvak-0172",
+      launch_date_local: "2020-08-30T19:18:00-04:00",
+      launch_site: {
+        site_name_long:
+          "Cape Canaveral Air Force Station Space Launch Complex 40",
+      },
+      links: {
+        article_link:
+          "https://spaceflightnow.com/2020/08/31/spacex-launches-first-polar-orbit-mission-from-florida-in-decades/",
+        video_link: "https://youtu.be/P-gLOsDjE3E",
+      },
+    },
+    {
+      mission_name: "Starlink-10 (v1.0) & SkySat 19-21",
+      launch_date_local: "2020-08-18T10:31:00-04:00",
+      launch_site: {
+        site_name_long:
+          "Cape Canaveral Air Force Station Space Launch Complex 40",
+      },
+      links: {
+        article_link:
+          "https://spaceflightnow.com/2020/08/18/spacex-adds-more-satellites-to-ever-growing-starlink-network/",
+        video_link: "https://youtu.be/jTMJK7wb0rM",
+      },
+    },
+  ],
+};
+
+const spaceXQuery = {launchesPast: ["mission_name","launch_date_local",{"launch_site": ["article_link"]},{"links": [ "article_link"]}]};
 const queryData = {project: ["project_id","project_name"]}
 
 const key_data = {
-  companies: ["company_id", "name", {"employees": ["user_id", "name"]}],
+  companies: ["company_id", "name", {"employees": ["user_id", "name", ]}],
 };
+// {companies: {"companies": true, "name": true,}}
 
 
-
-console.log(DuplicateFilter(data3,key_data));
-
+const result = DuplicateFilter(spaceX ,spaceXQuery);
+// console.log(result["launchesPast"]);
+console.log(result);
