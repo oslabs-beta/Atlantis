461828f9f00eecf13bd8a1508f9c2e8ecab43a36
coralfussman <coralfussman@gmail.com>
2021-06-16 16:01:38 -0400

    Adding wireframe story and content

--- Atlantis-Demo/client/containers/About.js
+++ Atlantis-Demo/client/containers/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import page_brk from '../assets/page_brk.svg'
-
+import atlantis_QL from '../assets/atlantis_QL.svg'
 
 
 function About() {
@@ -14,19 +14,51 @@ function About() {
 
   return (
     <>
-        <h2>Atlantis - The App</h2>
-        <img src={page_brk} />
-    <div className="about">
-     
-            <p>Atlantis is a light-weight library that leverages Redis <br/>
-                key-value store to dynamically cache GraphQL queries as responses. <br/>
-                Atlantis is able to dynamically store deeply-nested queries and maintain 
-                the most recent and relevant data as mutations are made to the database.<br/>
-                Queries that are more shallow and within the scope of previous queries 
-                are pulled directly from the cache,  offering further flexibility and <br/>
-                precision, without additional database requests or overriding previous key-values. </p>
-  
+    
+    
+    <div className="about-container">
+   
+        
+
+           <div className="about-left">
+               <h2>Atlantis is a light-weight library that solves the issue of storing and maintaining deeply-nested queries</h2>
+               <p>Ensuring the client always receives the most relevant data as mutations are made to the database. </p>
+           
+           </div>
+           <div className="about-right">
+               <h1>The Power of Atlantis</h1>
+               <img src={atlantis_QL} />
+           </div>
+          
     </div>
+    <div className="about-container">
+   
+        
+
+   <div className="redis-left">
+     
+       <img src="https://media.giphy.com/media/rok0zBiCcCMrMFvS9x/giphy.gif" width="300"/>
+   </div>
+   <div className="redis-right">
+   <h2>Queries that are more shallow and within the scope of previous queries 
+                are pulled directly from the cache,  offering further flexibility and 
+                precision, without additional database requests or overriding previous key-values.</h2>
+      
+  
+   </div>
+  
+</div>
+    
+            {/* s */}
+            {/* <p> Queries that are more shallow and within the scope of previous queries 
+                are pulled directly from the cache,  offering further flexibility and 
+                precision, without additional database requests or overriding previous key-values. </p>
+                <p> Atlantis that leverages Redis's 'in-memory' quick lookup time
+              to dynamically cache GraphQL queries as responses.</p> */}
+             
+       
+        <img src={page_brk} />
+       
     </>
   );
 }
